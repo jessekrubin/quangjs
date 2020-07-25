1d129351f4e6041d31a02c9ba3db2db340490fac
jessekrubin <jessekrubin@gmail.com>
2020-07-25 00:10:20 -0700

    update to thingy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * as quangjs from './quangjs'
-import * as quangjs from './quangjs'
+import * as quangjs from './quangjs';
+export * as quangjs from './quangjs';
 export default quangjs;
--- test/quangjs.test.ts
+++ test/quangjs.test.ts
@@ -1,10 +1,10 @@
 import quangjs from '../src';
-import * as qjs from '../src'
+import * as qjs from '../src';
 describe('basic import stuff', () => {
   it('default import/export', () => {
-    console.log(quangjs)
+    console.log(quangjs);
   });
   it('keyvals', () => {
-    console.log(qjs)
+    console.log(qjs);
   });
 });
